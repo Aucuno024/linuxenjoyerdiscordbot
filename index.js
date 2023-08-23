7df9cc3b50d8b2e285020785566c27ed24466d2e
Christianf67 <christianf67@laposte.net>
2023-08-23 23:28:37 +0200

    correct formatting

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js')
 const fs = require("fs")
 const client = new Discord.Client({ intents: Object.keys(Discord.GatewayIntentBits), partials: [Discord.Partials.Channel] })
 const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 require('dotenv').config()
@@ -16,28 +16,31 @@ client.on("ready", async () => {
         status: "idle"
     });
 });
+
 client.login(process.env.TOKEN).then(r => "Error")
-try{
+
+try {
     console.log(fs.readFileSync("webhook.txt", "utf-8").replace(" ", ""))
-}catch{
+} catch {
     readline.question('Quel est l\'id du salon ? >>>', id => {
         console.log(`${id}`);
         fs.writeFileSync("webhook.txt", id, "utf-8")
     readline.close();
 });
 }
-client.on("messageCreate",  async message =>{
-    if(message.content.toLowerCase().includes("window") || message.content.toLowerCase().includes("microsoft")){
+
+client.on("messageCreate",  async message => {
+    if(message.content.toLowerCase().includes("window") || message.content.toLowerCase().includes("microsoft")) {
         const chan =  message.guild.channels.cache.get(fs.readFileSync("webhook.txt", "utf-8").replace(" ", ""));
         let webhooks = await chan.fetchWebhooks();
         let webhook = webhooks.find(wh => wh.token)
         let msg = message.content
         console.log("Etape 1")
-    if(message.channel.id !== webhook.channel.id){
-        await webhook.edit({
-        channel: message.channel
-        })
-    }
+        if(message.channel.id !== webhook.channel.id) {
+            await webhook.edit({
+                channel: message.channel
+            })
+        }
         console.log("Etape 2")
         msg = msg.replaceAll("Microsoft", "Meinkrosauft")
         msg = msg.replaceAll("microsoft", "meinkrosauft")
