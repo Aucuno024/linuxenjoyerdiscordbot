3a50d555e5fd3e599f16d3e294b437e4e0fe0eda
SDK4rma <matt.michalet@gmail.com>
2023-09-02 16:30:47 +0200

    Changed "Elon MessUp" to "Elon Meusp"

--- pipelines/linuxenjoyer/4-ElonMusk.js
+++ pipelines/linuxenjoyer/4-ElonMusk.js
@@ -1,12 +1,12 @@
 module.exports.enabled = true
 
 module.exports.accept = (message, msg) => {
-    if(message.content.toLowerCase().includes("elon musk")){
-        let msg = message.content.replaceAll("Elon Musk", "Elon MessUp")
-        msg = msg.replaceAll("Elon musk", "Elon messup")
-        msg = msg.replaceAll("elon Musk", "elon MessUp")
-        msg = msg.replaceAll("elon musk", "elon messup")
-        msg = msg.replaceAll("ELON MUSK", "ELON MESSUP")
+        if(message.content.toLowerCase().includes("musk")){
+            let msg = message.content.replaceAll("Musk", "Meusp")
+            msg = msg.replaceAll("musk", "meusp")
+            msg = msg.replaceAll("Musk", "Meusp")
+            msg = msg.replaceAll("musk", "meusp")
+            msg = msg.replaceAll("MUSK", "MEUSP")
         return [message, msg]
     }else{
         return [message, msg]
