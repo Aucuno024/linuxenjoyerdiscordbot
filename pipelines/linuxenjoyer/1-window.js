5f03df7d61b2bf6b49efd75c5b059cb521bd9cf4
Chloé <51272699+Aucuno024@users.noreply.github.com>
2023-09-06 22:39:43 +0200

    edit window

--- pipelines/linuxenjoyer/1-window.js
+++ pipelines/linuxenjoyer/1-window.js
@@ -1,10 +1,10 @@
 module.exports.enabled = true
 
 module.exports.accept = (message) => {
-    if(message.content.toLowerCase().includes("windows")){
-        let msg = message.content.replaceAll("Windows", "Windaube")
-        msg = msg.replaceAll("windows", "windaube")
-        msg = msg.replaceAll("WINDOWS", "WINDAUBE")
+    if(message.content.toLowerCase().includes("window")){
+        let msg = message.content.replaceAll("Window", "Windaube")
+        msg = msg.replaceAll("window", "windaube")
+        msg = msg.replaceAll("WINDOW", "WINDAUBE")
         return [message, msg]
     }else{
         return [message, message.content]
