8eee7351fb4a80c637131415af617ba9dd7cdad7
Christianf67 <christianf67@laposte.net>
2023-12-30 00:06:31 +0100

    Update dailyHello.js

--- dailyHello.js
+++ dailyHello.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto")
+const fs = require("fs")
 
 let bot;
 function launchHelloTask(client) {
@@ -11,7 +12,7 @@ function helloTask() {
     const now = new Date()
     const nextMidnight = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 0, 0, 0)
     const gifList = gifCache[now.getDay()]
-    const guildChannelCache = Object.entries(require("./helloChannel.json"))
+    const guildChannelCache = Object.entries(JSON.parse(fs.readFileSync("./helloChannel.json", {encoding: "utf-8"})))
     for (const tuple of guildChannelCache) {
         const gif = gifList[crypto.randomInt(gifList.length)]
         const channel = bot.channels.cache.get(tuple[1])
